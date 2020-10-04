22e47e106a8fe2759aca846a05cf9bc7ec68a8ad
HaiderNoon <u2057829@unimail.hud.ac.uk>
2020-10-04 10:37:35 +0100

    Demo

---
+++ src/helloworld.java
@@ -0,0 +1,14 @@
+/**
+ * A demo class
+ * @author Haider Noon
+ * @version september 2020
+ */
+public class helloworld {
+    /**
+     * A demo method
+     * @param args
+     */
+    public static void main (String[] args){
+        System.out.println("Hello World");
+    }
+}
